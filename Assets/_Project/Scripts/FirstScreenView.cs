bbd0107c0ae16487e6b11681678c500be8784b5a
Berkan <berkan_mandaci@hotmail.com>
2024-08-14 19:16:08 +0300

    no message

---
+++ Assets/_Project/Scripts/FirstScreenView.cs
@@ -0,0 +1,30 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+namespace _Project.Scripts
+{
+    public class FirstScreenView : MonoBehaviour
+    {
+        [SerializeField] private Slider _slider;
+        [SerializeField] private GameObject _payButton;
+        private void Awake()
+        {
+            _slider.gameObject.SetActive(true);
+            _payButton.SetActive(false);
+        }
+        private void Start()
+        {
+            _slider.DOValue(1, 2).OnComplete(() =>
+            {
+                _slider.gameObject.SetActive(false);
+                _payButton.gameObject.SetActive(true);
+            });
+        }
+
+        public void OnClickToWin()
+        {
+            Debug.Log("Open steam api");
+        }
+    }
+}
